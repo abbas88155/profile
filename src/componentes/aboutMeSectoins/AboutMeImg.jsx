59c114ab0a04376cab813d9c1c98d42f0486bdde
abbas <abbasjawadi88@gmail.com>
2025-07-11 16:33:18 -0700

    update image paths and deploy fixes

--- src/componentes/aboutMeSectoins/AboutMeImg.jsx
+++ src/componentes/aboutMeSectoins/AboutMeImg.jsx
@@ -5,7 +5,7 @@ function AboutMeImg() {
   return (
     <div className='h-[500px] w-[300px] relative sm:mt-11'>
         <div className='h-[500px] w-[300px] overflow-hidden absolute z-10'>
-            <motion.img variants={fadeIn('up', 0.2)} initial='hidden' whileInView='show' className='h-full w-auto object-cover rounded-[120px]' src="../../../public/images/about-me.jpg" alt=" about me image" />
+            <motion.img variants={fadeIn('up', 0.2)} initial='hidden' whileInView='show' className='h-full w-auto object-cover rounded-[120px]' src="/images/about-me.jpg" alt=" about me image" />
         </div>
         <div className='h-[500px] w-[250px] absolute left-[-30px] bottom-[-30px] bg-orange rounded-bl-[120px] rounded-tr-[120px] rounded-br-[20px] rounded-tl-[30px]'></div>
     </div>
--- src/componentes/skills/ImgSkills.jsx
+++ src/componentes/skills/ImgSkills.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ImgSkills() {
   return (
     <div className='w-full md:mt-[70px] lg:mt-16 z-20 absolute'>
-        <img src="../../../public/images/subSkills.jpg" alt="" />
+        <img src="/images/subSkills.jpg" alt="" />
     </div>
   )
 }
